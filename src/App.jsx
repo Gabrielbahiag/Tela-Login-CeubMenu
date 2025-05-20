fe675300a33dfe14363bd0756484d3cb44124102
Nicolas-Clay777 <nicolasclayoliveira@gmail.com>
2025-05-20 15:05:12 -0300

    CeubMenu-V3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import "./App.css";
-import Login from "./Components/Login";
+import './App.css';
+import Login from "./pages/login/Login";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Register from "./pages/register/Register";
 
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Cadastro" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
